52e1f2713d4c44feeb7d52512310d3a6ccf4df8c
[CLOUD4] 박진영 <wlsdud2528@naver.com>
2025-05-09 17:27:47 +0900

    refactor: SecurityConfig, WebConfig Netlify url 오리진 허용 추가

--- src/main/java/com/team2/mosoo_backend/config/WebConfig.java
+++ src/main/java/com/team2/mosoo_backend/config/WebConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    private static final String DEVELOP_FRONT_ADDRESS = "http://localhost:3000";
+    private static final String DEVELOP_FRONT_ADDRESS = "https://mo-soo.netlify.app";
 
     @Value("${DEPLOY_FRONT_URL}")
     private String DEPLOY_FRONT_URL;
